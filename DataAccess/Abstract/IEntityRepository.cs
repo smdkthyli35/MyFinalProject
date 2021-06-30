b3a7d074ffd92c0db8141f4a8fc5895ae651dd94
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-06-30 15:28:51 +0300

    8.gün sonu

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint : generic kısıtlamalar
+    //class : referans tip olabilir demek. class olmalı demek değil!
+    //IEntity : T ya IEntity olabilir ya da IEntity implemente eden bir nesne olabilir.
+    //new() : new'lenebilir olmalı. (IEntity interface olduğu için newlenemez!)
+    public interface IEntityRepository<T> where T:class,IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); //filtre vermeyedebilirsin demek.
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}

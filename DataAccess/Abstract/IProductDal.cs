c93c338a8e1e11187038f94e9d8469d1f6199f2a
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-07-01 18:15:31 +0300

    9. ders sonu.

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +10,8 @@ namespace DataAccess.Abstract
     public interface IProductDal:IEntityRepository<Product>
     {
         //interface'in oparasyonları default publicdir. ama kendisi default public değildir.
-       
+        List<ProductDetailDto> GetProductDetails();
     }
 }
+
+//Code Refactoring - Kodun iyileştirilmesi
